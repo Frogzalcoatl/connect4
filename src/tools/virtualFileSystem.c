cbeadeafea1a6b289f3d60da0e9e75ebeb86b771
Frogzalcoatl <saharholmes@gmail.com>
2026-01-24 11:12:25 -0500

    Copy assets folder on debug, run packer on release

--- src/tools/virtualFileSystem.c
+++ src/tools/virtualFileSystem.c
@@ -21,15 +21,36 @@ typedef struct C4_VFS_State {
     C4_PackEntry* entries;
     int fileCount;
     uint64_t dataStartOffset;
+    const char* basePath;
+    bool useFolder;
+    char datPath[2048];
 } C4_VFS_State;
 
 static C4_VFS_State vfs = {0};
+static void C4_VFS_InitThroughFolder(void) {
+    char assetsFolderPath[2048];
 
-void C4_VFS_Init(const char* filePath) {
-    assert(filePath);
+    SDL_snprintf(assetsFolderPath, sizeof(assetsFolderPath), "%s%s", vfs.basePath, "/assets");
 
-    vfs.packFile = SDL_IOFromFile(filePath, "rb");
+    SDL_PathInfo pathInfo;
+
+    bool getPathResult = SDL_GetPathInfo(assetsFolderPath, &pathInfo);
+
+    if (getPathResult && pathInfo.type == SDL_PATHTYPE_DIRECTORY) {
+        vfs.useFolder = true;
+        C4_Log("Initialized VFS using assets folder");
+        return;
+    }
+
+    vfs.useFolder = false;
+}
+
+static void C4_VFS_InitThroughDatFile(void) {
+    vfs.packFile = SDL_IOFromFile(vfs.datPath, "rb");
     if (!vfs.packFile) {
+        if (vfs.useFolder) {
+            return;
+        }
         C4_FatalError(C4_ErrorCode_DatFileMissing, " ");
     }
 
@@ -37,11 +58,18 @@ void C4_VFS_Init(const char* filePath) {
     SDL_ReadIO(vfs.packFile, &header, sizeof(C4_PackHeader));
 
     if (header.magic != C4_PACK_MAGIC) {
+        if (vfs.useFolder) {
+            return;
+        }
         C4_FatalError(C4_ErrorCode_DatFileInvalid, " ");
     }
 
     if (header.version != GAME_VERSION) {
-        C4_FatalError(C4_ErrorCode_DatFileInvalid, 
+        if (vfs.useFolder) {
+            return;
+        }
+        C4_FatalError(
+            C4_ErrorCode_DatFileInvalid, 
             "Expected version: %.1f\nFound version: %.1f", 
             GAME_VERSION, 
             header.version
@@ -60,7 +88,18 @@ void C4_VFS_Init(const char* filePath) {
     // Read the entries into RAM
     SDL_ReadIO(vfs.packFile, vfs.entries, entriesSize);
 
-   C4_Log("Initialized VFS using %s", filePath);
+    C4_Log("Initialized VFS using %s", vfs.datPath);
+}
+
+void C4_VFS_Init(const char* filePath) {
+    assert(filePath);
+
+    vfs.basePath = SDL_GetBasePath();
+    SDL_strlcpy(vfs.datPath, filePath, sizeof(vfs.datPath));
+
+    C4_VFS_InitThroughFolder();
+
+    C4_VFS_InitThroughDatFile();
 }
 
 void C4_VFS_Quit(void) {
@@ -79,11 +118,70 @@ void C4_VFS_Quit(void) {
     C4_Log("Quit VFS");
 }
 
+static void* C4_VFS_ReadFileFromFolder(const char* filename, size_t* outSize) {
+    assert(filename);
+
+    char fullFilePath[2048];
+    SDL_snprintf(fullFilePath, sizeof(fullFilePath), "%s%s%s", vfs.basePath, "/", filename);
+
+    // Using read binary "rb". "r" can corrupt binary data by translating line endings \n to \r\n
+    SDL_IOStream* io = SDL_IOFromFile(fullFilePath, "rb");
+    if (!io) {
+        return NULL;
+    }
+
+    Sint64 ioSize = SDL_GetIOSize(io);
+
+    if (ioSize < 0) {
+        SDL_CloseIO(io);
+        return NULL;
+    }
+
+    void* data = SDL_malloc(ioSize);
+    if (!data) {
+        C4_FatalError(C4_ErrorCode_OutOfMemory, " ");
+    }
+
+    SDL_ReadIO(io, data, ioSize);
+    SDL_CloseIO(io);
+
+    if (outSize) *outSize = ioSize;
+
+    return data;
+}
+
 void* C4_VFS_ReadFile(const char* filename, size_t* outSize) {
     assert(filename);
 
+    if (vfs.useFolder) {
+        void* result = C4_VFS_ReadFileFromFolder(filename, outSize);
+        if (result) {
+            return result;
+        }
+    }
+
+    if (!vfs.packFile) {
+        C4_VFS_InitThroughDatFile();
+    }
+
+    if (!vfs.packFile) {
+        C4_Warn(
+            SDL_LOG_CATEGORY_APPLICATION,
+            "Unable to read file %s",
+            filename
+        );
+        return NULL;
+    }
+
     uint64_t id = stringHash(filename);
     C4_PackEntry* entry = NULL;
+
+    if (!vfs.entries) {
+        C4_Warn(SDL_LOG_CATEGORY_APPLICATION, "Unable to access .dat file entries. Attempting to reinitialize dat file");
+        if (!vfs.entries) {
+            return NULL;
+        }
+    }
     
     for (int i = 0; i < vfs.fileCount; i++) {
         if (vfs.entries[i].id == id) {
